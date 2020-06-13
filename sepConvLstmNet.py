3bb2963240d3718a85962bd5e81fdcc6eb0fdb9c
Zedd1558 <zahidulislam@iut-dhaka.edu>
2020-06-13 14:04:41 +0600

    stacked residual lstms - 2 lstm layers - 86.50%

--- sepConvLstmNet.py
+++ sepConvLstmNet.py
@@ -19,50 +19,53 @@ def getModel(size=224, seq_len=32 , cnn_weight = 'imagenet',cnn_trainable = True
        returns:
     model
     """
-    inputs = Input(shape=(seq_len, size, size, 6),name='Input')
+    inputs = Input(shape=(seq_len, size, size, 6),name='input')
     
-    image_input = Lambda(lambda x: x[...,:3]  , name='ImageInput', output_shape=(seq_len, size, size, 3))(inputs)
-    diff_input = Lambda( lambda x: x[...,3:] , name='DiffInput', output_shape=(seq_len, size, size, 3))(inputs)
+    frames_input = Lambda( lambda x: x[...,:3]  , name='frames_input', output_shape=(seq_len, size, size, 3) ) (inputs)
+    frames_diff_input = Lambda( lambda x: x[...,3:] , name='frames_diff_input', output_shape=(seq_len, size, size, 3) ) (inputs)
     
-    cnn = MobileNetV2( input_shape=(size,size,3), alpha=0.35, weights='imagenet', include_top=False)
-    cnn = Model(inputs=[cnn.layers[0].input],outputs=[cnn.layers[-30].output]) # taking only upto block 13
+    frames_cnn = MobileNetV2( input_shape = (size,size,3), alpha=0.35, weights='imagenet', include_top=False )
+    frames_cnn = Model( inputs=[frames_cnn.layers[0].input],outputs=[frames_cnn.layers[-30].output] ) # taking only upto block 13
 
     print('> cnn_trainable : ', cnn_trainable)
-    for layer in cnn.layers:
+    for layer in frames_cnn.layers:
         layer.trainable = cnn_trainable
 
-    diff_cnn = MobileNetV2( input_shape=(size,size,3), alpha=0.35, weights='imagenet', include_top=False)
-    diff_cnn = Model(inputs=[diff_cnn.layers[0].input],outputs=[diff_cnn.layers[-30].output]) # taking only upto block 13
+    frames_diff_cnn = MobileNetV2( input_shape=(size,size,3), alpha=0.35, weights='imagenet', include_top = False )
+    frames_diff_cnn = Model( inputs = [frames_diff_cnn.layers[0].input], outputs = [frames_diff_cnn.layers[-30].output] ) # taking only upto block 13
     
     print('> cnn_trainable : ', cnn_trainable)
-    for layer in diff_cnn.layers:
+    for layer in frames_diff_cnn.layers:
         layer.trainable = cnn_trainable
 
-    cnn = TimeDistributed( cnn,name='frame_CNN' )(image_input)
-    cnn = TimeDistributed( LeakyReLU(alpha=0.1), name='leaky_relu_1' )(cnn)
-    cnn = TimeDistributed( Dropout(0.2) ,name='dropout1' )(cnn)
+    frames_cnn = TimeDistributed( frames_cnn,name='frames_CNN' )( frames_input )
+    frames_cnn = TimeDistributed( LeakyReLU(alpha=0.1), name='leaky_relu_1_' )( frames_cnn)
+    frames_cnn = TimeDistributed( Dropout(0.2) ,name='dropout_1_' )(frames_cnn)
   
-    diff_cnn = TimeDistributed( diff_cnn,name='diff_CNN' )(diff_input)
-    diff_cnn = TimeDistributed( LeakyReLU(alpha=0.1), name='leaky_relu_2' )(diff_cnn)
-    diff_cnn = TimeDistributed( Dropout(0.2) ,name='dropout2' )(diff_cnn)
+    frames_diff_cnn = TimeDistributed( frames_diff_cnn,name='frames_diff_CNN' )(frames_diff_input)
+    frames_diff_cnn = TimeDistributed( LeakyReLU(alpha=0.1), name='leaky_relu_2_' )(frames_diff_cnn)
+    frames_diff_cnn = TimeDistributed( Dropout(0.2) ,name='dropout_2_' )(frames_diff_cnn)
 
-    cnn = Concatenate(axis=-1, name='concatenate_')([cnn, diff_cnn])
+    cnn = Concatenate(axis=-1, name='concatenate_')([frames_cnn, frames_diff_cnn])
 
     lstm = SepConvLSTM2D( filters = 128, kernel_size=(3, 3), padding='same', return_sequences=True, dropout=0.4, recurrent_dropout=0.4, name='SepConvLSTM2D_1', kernel_regularizer=l2(weight_decay), recurrent_regularizer=l2(weight_decay))(cnn)
     lstm_0 = BatchNormalization( axis = 4 )(lstm)
+    lstm = SepConvLSTM2D( filters = 128, kernel_size=(3, 3), padding='same', return_sequences=True, dropout=0.4, recurrent_dropout=0.4, name='SepConvLSTM2D_2', kernel_regularizer=l2(weight_decay), recurrent_regularizer=l2(weight_decay))(lstm_0)
+    lstm = BatchNormalization( axis = 4 )(lstm)
+    lstm = Add(name="residul_connection")([lstm, lstm_0])
 
     x = Conv3D(
-        64, kernel_size=(1,3,3), strides=(1,1,1), kernel_initializer='he_normal', activation='relu', padding='same',kernel_regularizer=l2(weight_decay))(lstm)
+        64, kernel_size=(1,3,3), strides=(1,1,1), kernel_initializer='he_normal', activation='relu', padding='same',kernel_regularizer=l2(weight_decay),name='3d_conv_1')(lstm)
    
     x = MaxPooling3D(pool_size=(8,1,1))(x)
     
     x = Conv3D(
-        64, kernel_size=(3,1,1), strides=(1,1,1), kernel_initializer='he_normal', activation='relu', padding='same',kernel_regularizer=l2(weight_decay))(x)
+        64, kernel_size=(3,1,1), strides=(1,1,1), kernel_initializer='he_normal', activation='relu', padding='same',kernel_regularizer=l2(weight_decay),name='3d_conv_2')(x)
   
     x = MaxPooling3D(pool_size=(2,2,2))(x)
 
     x = Conv3D(
-        64, kernel_size=(1,3,3), strides=(1,1,1), kernel_initializer='he_normal', activation='relu', padding='same',kernel_regularizer=l2(weight_decay))(x)
+        64, kernel_size=(1,3,3), strides=(1,1,1), kernel_initializer='he_normal', activation='relu', padding='same',kernel_regularizer=l2(weight_decay), name="3d_conv_3")(x)
     
     x = MaxPooling3D(pool_size=(1,3,3))(x)
 
