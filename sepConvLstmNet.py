7c0fb88095e70d5ad23ea7e17e2c7ebf1a1a3845
Zedd1558 <zahidulislam@iut-dhaka.edu>
2020-07-28 09:38:06 +0600

    lstm dropout 0.1, dense dropout 0.3, dense leakyrelu alpha reduced

--- sepConvLstmNet.py
+++ sepConvLstmNet.py
@@ -23,7 +23,7 @@ def getModel(size=224, seq_len=32 , cnn_weight = 'imagenet',cnn_trainable = True
     frames_input = Input(shape=(seq_len, size, size, 3),name='frames_input')
     frames_diff_input = Input(shape=(seq_len - frame_diff_interval, size, size, 3),name='frames_diff_input')
     
-    frames_cnn = MobileNetV2( input_shape = (size,size,3), alpha=0.35, weights='imagenet', include_top=False )
+    frames_cnn = MobileNetV2( input_shape = (size,size,3), alpha=0.35, weights='imagenet', include_top = False )
     frames_cnn = Model( inputs=[frames_cnn.layers[0].input],outputs=[frames_cnn.layers[-30].output] ) # taking only upto block 13
 
     print('> cnn_trainable : ', cnn_trainable)
@@ -45,24 +45,24 @@ def getModel(size=224, seq_len=32 , cnn_weight = 'imagenet',cnn_trainable = True
     frames_diff_cnn = TimeDistributed( LeakyReLU(alpha=0.1), name='leaky_relu_2_' )(frames_diff_cnn)
     frames_diff_cnn = TimeDistributed( Dropout(0.25) ,name='dropout_2_' )(frames_diff_cnn)
 
-    frames_lstm = SepConvLSTM2D( filters = 64, kernel_size=(3, 3), padding='same', return_sequences=False, dropout=0.2, recurrent_dropout=0.2, name='SepConvLSTM2D_1', kernel_regularizer=l2(weight_decay), recurrent_regularizer=l2(weight_decay))(frames_cnn)
+    frames_lstm = SepConvLSTM2D( filters = 64, kernel_size=(3, 3), padding='same', return_sequences=False, dropout=0.1, recurrent_dropout=0.1, name='SepConvLSTM2D_1', kernel_regularizer=l2(weight_decay), recurrent_regularizer=l2(weight_decay))(frames_cnn)
     frames_lstm = BatchNormalization( axis = -1 )(frames_lstm)
 
-    frames_diff_lstm = SepConvLSTM2D( filters = 64, kernel_size=(3, 3), padding='same', return_sequences=False, dropout=0.2, recurrent_dropout=0.2, name='SepConvLSTM2D_2', kernel_regularizer=l2(weight_decay), recurrent_regularizer=l2(weight_decay))(frames_diff_cnn)
+    frames_diff_lstm = SepConvLSTM2D( filters = 64, kernel_size=(3, 3), padding='same', return_sequences=False, dropout=0.1, recurrent_dropout=0.1, name='SepConvLSTM2D_2', kernel_regularizer=l2(weight_decay), recurrent_regularizer=l2(weight_decay))(frames_diff_cnn)
     frames_diff_lstm = BatchNormalization( axis = -1 )(frames_diff_lstm)
 
     lstm = Concatenate(axis=-1, name='concatenate_')([frames_lstm, frames_diff_lstm])
     
-    lstm = AveragePooling2D((2,2) , name = 'max_pooling_')(lstm)
+    lstm = MaxPooling2D((2,2) , name = 'max_pooling_')(lstm)
     
     x = Flatten()(lstm) 
   
     dropout = 0.3
     x = Dense(128)(x)
-    x = LeakyReLU(alpha=0.3)(x)
+    x = LeakyReLU(alpha=0.2)(x)
     x = Dropout(dropout)(x)
     x = Dense(16)(x)
-    x = LeakyReLU(alpha=0.2)(x)
+    x = LeakyReLU(alpha=0.1)(x)
     x = Dropout(dropout)(x)
     predictions = Dense(1, activation='sigmoid')(x)
     
