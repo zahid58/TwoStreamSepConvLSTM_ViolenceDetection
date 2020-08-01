28ead861c95426ccdda40b1be1a3351e037c418c
Zedd1558 <zahidulislam@iut-dhaka.edu>
2020-08-02 01:47:36 +0600

    trying out cnn-48 stacked

--- sepConvLstmNet.py
+++ sepConvLstmNet.py
@@ -24,14 +24,14 @@ def getModel(size=224, seq_len=32 , cnn_weight = 'imagenet',cnn_trainable = True
     frames_diff_input = Input(shape=(seq_len - frame_diff_interval, size, size, 3),name='frames_diff_input')
     
     frames_cnn = MobileNetV2( input_shape = (size,size,3), alpha=0.35, weights='imagenet', include_top = False )
-    frames_cnn = Model( inputs=[frames_cnn.layers[0].input],outputs=[frames_cnn.layers[-30].output] ) # taking only upto block 13
+    frames_cnn = Model( inputs=[frames_cnn.layers[0].input],outputs=[frames_cnn.layers[-48].output] ) # taking only upto block 13
 
     print('> cnn_trainable : ', cnn_trainable)
     for layer in frames_cnn.layers:
         layer.trainable = cnn_trainable
 
     frames_diff_cnn = MobileNetV2( input_shape=(size,size,3), alpha=0.35, weights='imagenet', include_top = False )
-    frames_diff_cnn = Model( inputs = [frames_diff_cnn.layers[0].input], outputs = [frames_diff_cnn.layers[-30].output] ) # taking only upto block 13
+    frames_diff_cnn = Model( inputs = [frames_diff_cnn.layers[0].input], outputs = [frames_diff_cnn.layers[-48].output] ) # taking only upto block 13
     
     print('> cnn_trainable : ', cnn_trainable)
     for layer in frames_diff_cnn.layers:
@@ -45,16 +45,20 @@ def getModel(size=224, seq_len=32 , cnn_weight = 'imagenet',cnn_trainable = True
     frames_diff_cnn = TimeDistributed( LeakyReLU(alpha=0.1), name='leaky_relu_2_' )(frames_diff_cnn)
     frames_diff_cnn = TimeDistributed( Dropout(0.25) ,name='dropout_2_' )(frames_diff_cnn)
 
-    frames_lstm = SepConvLSTM2D( filters = 64, kernel_size=(3, 3), padding='same', return_sequences=True, dropout=0.1, recurrent_dropout=0.1, name='SepConvLSTM2D_1', kernel_regularizer=l2(weight_decay), recurrent_regularizer=l2(weight_decay))(frames_cnn)
-    frames_lstm = BatchNormalization( axis = -1 )(frames_lstm)
+    lstm_dropout=0.2
 
-    frames_lstm = SepConvLSTM2D( filters = 64, kernel_size=(3, 3), padding='same', return_sequences=False, dropout=0.1, recurrent_dropout=0.1, name='SepConvLSTM2D_1', kernel_regularizer=l2(weight_decay), recurrent_regularizer=l2(weight_decay))(frames_lstm)
+    frames_lstm = SepConvLSTM2D( filters = 64, kernel_size=(3, 3), padding='valid', return_sequences=True, dropout=lstm_dropout, recurrent_dropout=lstm_dropout, name='SepConvLSTM2D_1a', kernel_regularizer=l2(weight_decay), recurrent_regularizer=l2(weight_decay))(frames_cnn)
+    frames_lstm = BatchNormalization( axis = -1 )(frames_lstm)
+    frames_lstm = TimeDistributed( MaxPooling2D((2,2)) , name = 'max_pooling_lstm_1' )(frames_lstm)
+
+    frames_lstm = SepConvLSTM2D( filters = 64, kernel_size=(3, 3), padding='same', return_sequences=False, dropout=lstm_dropout, recurrent_dropout=lstm_dropout, name='SepConvLSTM2D_1b', kernel_regularizer=l2(weight_decay), recurrent_regularizer=l2(weight_decay))(frames_lstm)
     frames_lstm = BatchNormalization( axis = -1 )(frames_lstm)
     
-    frames_diff_lstm = SepConvLSTM2D( filters = 64, kernel_size=(3, 3), padding='same', return_sequences=True, dropout=0.1, recurrent_dropout=0.1, name='SepConvLSTM2D_2', kernel_regularizer=l2(weight_decay), recurrent_regularizer=l2(weight_decay))(frames_diff_cnn)
+    frames_diff_lstm = SepConvLSTM2D( filters = 64, kernel_size=(3, 3), padding='valid', return_sequences=True, dropout=lstm_dropout, recurrent_dropout=lstm_dropout, name='SepConvLSTM2D_2a', kernel_regularizer=l2(weight_decay), recurrent_regularizer=l2(weight_decay))(frames_diff_cnn)
     frames_diff_lstm = BatchNormalization( axis = -1 )(frames_diff_lstm)
+    frames_diff_lstm = TimeDistributed( MaxPooling2D((2,2)) , name = 'max_pooling_lstm_2' )(frames_diff_lstm)
 
-    frames_diff_lstm = SepConvLSTM2D( filters = 64, kernel_size=(3, 3), padding='same', return_sequences=False, dropout=0.1, recurrent_dropout=0.1, name='SepConvLSTM2D_2', kernel_regularizer=l2(weight_decay), recurrent_regularizer=l2(weight_decay))(frames_diff_lstm)
+    frames_diff_lstm = SepConvLSTM2D( filters = 64, kernel_size=(3, 3), padding='same', return_sequences=False, dropout=lstm_dropout, recurrent_dropout=lstm_dropout, name='SepConvLSTM2D_2b', kernel_regularizer=l2(weight_decay), recurrent_regularizer=l2(weight_decay))(frames_diff_lstm)
     frames_diff_lstm = BatchNormalization( axis = -1 )(frames_diff_lstm)
 
     lstm = Concatenate(axis=-1, name='concatenate_')([frames_lstm, frames_diff_lstm])
