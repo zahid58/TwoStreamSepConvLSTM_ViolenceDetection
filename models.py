137c18459c8f2b27bb530e04ca75c3a8e54113d5
Zedd1558 <zahidulislam@iut-dhaka.edu>
2021-04-17 17:43:26 +0600

    ddd

--- models.py
+++ models.py
@@ -235,9 +235,6 @@ def getProposedModelM(size=224, seq_len=32 , cnn_weight = 'imagenet',cnn_trainab
     return model
 
 
-
-
-
 def getProposedModelA(size=224, seq_len=32 , cnn_weight = 'imagenet',cnn_trainable = True, lstm_type='sepconv', weight_decay = 2e-5, frame_diff_interval = 1, mode = "both", cnn_dropout = 0.25, lstm_dropout = 0.25, dense_dropout = 0.3, seed = 42):
     """parameters:
     size = height/width of each frame,
@@ -342,8 +339,6 @@ def getProposedModelA(size=224, seq_len=32 , cnn_weight = 'imagenet',cnn_trainab
     return model
 
 
-
-
 def conv3d_block(input_, name_):
     x = input_
     x = Conv3D(
@@ -365,105 +360,3 @@ def conv3d_block(input_, name_):
     x = MaxPooling3D(pool_size=(1,3,3))(x)
 
     return Model(inputs = input_, outputs = x, name = name_)
-
-def getFlowGatedNet():
-
-    # extract the rgb images 
-    def get_rgb(input_x):
-        rgb = input_x[...,:3]
-        return rgb
-
-    # extract the optical flows
-    def get_opt(input_x):
-        opt= input_x[...,3:5]
-        return opt
-
-    inputs = Input(shape=(64,224,224,5))
-
-    rgb = Lambda(get_rgb,output_shape=None)(inputs)
-    opt = Lambda(get_opt,output_shape=None)(inputs)
-
-    ##################################################### RGB channel
-    rgb = Conv3D(
-        16, kernel_size=(1,3,3), strides=(1,1,1), kernel_initializer='he_normal', activation='relu', padding='same')(rgb)
-    rgb = Conv3D(
-        16, kernel_size=(3,1,1), strides=(1,1,1), kernel_initializer='he_normal', activation='relu', padding='same')(rgb)
-    rgb = MaxPooling3D(pool_size=(1,2,2))(rgb)
-
-    rgb = Conv3D(
-        16, kernel_size=(1,3,3), strides=(1,1,1), kernel_initializer='he_normal', activation='relu', padding='same')(rgb)
-    rgb = Conv3D(
-        16, kernel_size=(3,1,1), strides=(1,1,1), kernel_initializer='he_normal', activation='relu', padding='same')(rgb)
-    rgb = MaxPooling3D(pool_size=(1,2,2))(rgb)
-
-    rgb = Conv3D(
-        32, kernel_size=(1,3,3), strides=(1,1,1), kernel_initializer='he_normal', activation='relu', padding='same')(rgb)
-    rgb = Conv3D(
-        32, kernel_size=(3,1,1), strides=(1,1,1), kernel_initializer='he_normal', activation='relu', padding='same')(rgb)
-    rgb = MaxPooling3D(pool_size=(1,2,2))(rgb)
-
-    rgb = Conv3D(
-        32, kernel_size=(1,3,3), strides=(1,1,1), kernel_initializer='he_normal', activation='relu', padding='same')(rgb)
-    rgb = Conv3D(
-        32, kernel_size=(3,1,1), strides=(1,1,1), kernel_initializer='he_normal', activation='relu', padding='same')(rgb)
-    rgb = MaxPooling3D(pool_size=(1,2,2))(rgb)
-
-    ##################################################### Optical Flow channel
-    opt = Conv3D(
-        16, kernel_size=(1,3,3), strides=(1,1,1), kernel_initializer='he_normal', activation='relu', padding='same')(opt)
-    opt = Conv3D(
-        16, kernel_size=(3,1,1), strides=(1,1,1), kernel_initializer='he_normal', activation='relu', padding='same')(opt)
-    opt = MaxPooling3D(pool_size=(1,2,2))(opt)
-
-    opt = Conv3D(
-        16, kernel_size=(1,3,3), strides=(1,1,1), kernel_initializer='he_normal', activation='relu', padding='same')(opt)
-    opt = Conv3D(
-        16, kernel_size=(3,1,1), strides=(1,1,1), kernel_initializer='he_normal', activation='relu', padding='same')(opt)
-    opt = MaxPooling3D(pool_size=(1,2,2))(opt)
-
-    opt = Conv3D(
-        32, kernel_size=(1,3,3), strides=(1,1,1), kernel_initializer='he_normal', activation='relu', padding='same')(opt)
-    opt = Conv3D(
-        32, kernel_size=(3,1,1), strides=(1,1,1), kernel_initializer='he_normal', activation='relu', padding='same')(opt)
-    opt = MaxPooling3D(pool_size=(1,2,2))(opt)
-
-    opt = Conv3D(
-        32, kernel_size=(1,3,3), strides=(1,1,1), kernel_initializer='he_normal', activation='sigmoid', padding='same')(opt)
-    opt = Conv3D(
-        32, kernel_size=(3,1,1), strides=(1,1,1), kernel_initializer='he_normal', activation='sigmoid', padding='same')(opt)
-    opt = MaxPooling3D(pool_size=(1,2,2))(opt)
-
-
-    ##################################################### Fusion and Pooling
-    x = Multiply()([rgb,opt])
-    x = MaxPooling3D(pool_size=(8,1,1))(x)
-
-    ##################################################### Merging Block
-    x = Conv3D(
-        64, kernel_size=(1,3,3), strides=(1,1,1), kernel_initializer='he_normal', activation='relu', padding='same')(x)
-    x = Conv3D(
-        64, kernel_size=(3,1,1), strides=(1,1,1), kernel_initializer='he_normal', activation='relu', padding='same')(x)
-    x = MaxPooling3D(pool_size=(2,2,2))(x)
-
-    x = Conv3D(
-        64, kernel_size=(1,3,3), strides=(1,1,1), kernel_initializer='he_normal', activation='relu', padding='same')(x)
-    x = Conv3D(
-        64, kernel_size=(3,1,1), strides=(1,1,1), kernel_initializer='he_normal', activation='relu', padding='same')(x)
-    x = MaxPooling3D(pool_size=(2,2,2))(x)
-
-    x = Conv3D(
-        128, kernel_size=(1,3,3), strides=(1,1,1), kernel_initializer='he_normal', activation='relu', padding='same')(x)
-    x = Conv3D(
-        128, kernel_size=(3,1,1), strides=(1,1,1), kernel_initializer='he_normal', activation='relu', padding='same')(x)
-    x = MaxPooling3D(pool_size=(2,3,3))(x)
-
-    ##################################################### FC Layers
-    x = Flatten()(x)
-    x = Dense(128,activation='relu')(x)
-    x = Dropout(0.2)(x)
-    x = Dense(32, activation='relu')(x)
-
-    # Build the model
-    pred = Dense(2, activation='softmax')(x)
-    model = Model(inputs=inputs, outputs=pred)
-    return model
